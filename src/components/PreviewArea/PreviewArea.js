8d6259c5624e33f98b657dde46978a4314670339
Eric Wang <eric.wang@amplitude.com>
2018-12-04 19:26:13 -0800

    implement image download

--- src/components/PreviewArea/PreviewArea.js
+++ src/components/PreviewArea/PreviewArea.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
+import mergeImages from 'merge-images';
+
 import { categories, decorations } from '../../lib/asset_config'
 
 import './PreviewArea.css';
 
 class PreviewArea extends Component {
-  renderDecoration(category, decoration) {
-    const categoryName = categories[category];
-    const decorationName = decorations[categoryName][decoration];
-    const path = `/assets/${categoryName}/${decorationName}.svg`;
-    return <img src={path} />;
+  state = {
+    srcImage: null,
+  }
+
+  setSrcImage() {
+    const { props } = this;
+
+    const base_image = ['assets/base_datamonster_tail_left.svg']
+    const decoration_images = props.decorations.map((decoration, category) => {
+      const categoryName = categories[category];
+      const decorationName = decorations[categoryName][decoration];
+      return `assets/${categoryName}/${decorationName}.svg`
+    });
+
+    const images = base_image.concat(decoration_images);
+
+    mergeImages(images, {format: 'image/svg+xml'})
+      .then(src => {
+        this.setState({srcImage: src});
+      });
+  }
+
+  componentDidMount() {
+    this.setSrcImage();
+  }
+
+  componentDidUpdate() {
+    this.setSrcImage();
   }
 
   render() {
-    const { props } = this;
+    const { state } = this;
 
     return (
       <div className="PreviewArea">
-        <img src="assets/base_datamonster_tail_left.svg" />
-        {props.decorations.map((decoration, category) => {
-          return this.renderDecoration(category, decoration);
-        })}
+        <img src={state.srcImage} />
+        <a href={state.srcImage} download>Click to download</a>
       </div>
     );
   }
